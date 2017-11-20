c8be3b82e79d1075ade6a0d29cc519951b0857b4
shittybill <shittybill@bluntforce>
2017-11-19 19:27:46 -0500

    tweak to regex

--- commands/command.registry.js
+++ commands/command.registry.js
@@ -12,15 +12,17 @@ class CommandRegistry {
     	
     	try {
 	    	//Allow admin to post anything
-	    	if( this.message.member.roles.find("name", this.config.settings.adminRole) || this.message.author.id === this.config.settings.master ) { return; }			
+	    	if( this.message.member.roles.find("name", this.config.settings.adminRole) /*|| this.message.author.id === this.config.settings.master*/ ) { return; }			
 	    		
 	    	//Check user message
 	        if (this.message.content) {
 	        	
-	            let splittingPattern = /[\w|\s|+-.,!@#$%^&*();\/|<>"']+[?]/gm;
+	            let splittingPattern = /([\w][\w\s\d\,\.\'\"\“\”]+)[\?]/gm;
 	            let msgArray = this.message.content.match(splittingPattern);
 	            
-	            if ( typeof msgArray === "undefined" || msgArray === null ) {
+	            console.log( msgArray );
+	            
+	            if ( typeof msgArray === "undefined" || msgArray === null || msgArray.length === 0 ) {
 	            	
 	                let command = msgArray;
 	                let noCommandsFound = false;
